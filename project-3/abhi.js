24901147a1680bdcd365fbe64a60e698c1d6663d
Abhishek <abhikannor12@gmail.com>
2024-02-03 13:59:01 +0530

    my third project

---
+++ project-3/abhi.js
@@ -0,0 +1,18 @@
+
+let clickBox1 = document.querySelector(".box1");
+
+clickBox1.addEventListener("click",function(){
+    document.body.style.backgroundColor="blue"
+})
+
+let clickBox2 = document.querySelector(".box2");
+
+clickBox2.addEventListener("click",function(){
+    document.body.style.backgroundColor="black"
+})
+
+let clickBox3 = document.querySelector(".box3");
+
+clickBox3.addEventListener("click",function(){
+    document.body.style.backgroundColor="yellow"
+})
